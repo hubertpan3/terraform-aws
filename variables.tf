4473e3c55ce10a9ef8935fa925b71389e8491199
hubertpan3 <hubert.pan3@gmail.com>
2023-06-19 03:02:13 -0400

    Update variables.tf
    add comment on how variables can be overridden

--- variables.tf
+++ variables.tf
@@ -1,4 +1,5 @@
 variable "deployment_tag" {
+  # can be overriden at apply time with `-var "deployment_tag=<insert-new-val-here>`
   description = "Value of the deployment tag for resources"
   type = string
   default = "deploymentTagVal"
